4647cf769247381942d1726d205b166e94de9fe3
RenzoVyt <91734371+RenzoVyt@users.noreply.github.com>
2021-10-01 21:21:37 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## 
+## input x as matrix
+## set every solved value "a" as null
+## changed every "mean" to "solve"
+jorscatxxsCachematrix <- function(x = matrix(sample(59:5512,70),92,85) {
+        a <- NULL
+        metajoksx <- function(y){
+                x <<- y
+                a <<- NULL
+                }
+        phantomxs <- function() x 
+        metajoksphantomxs <- function(solve) a <<- inverse
+        metajoksphantomxs <- function() s 
+        list (set = set,
+              get = get,
+              setmean = setmean,
+              getmean = getmean)
+        
+                        
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## input x as matrix
+## set "a" as null
+## change "mean" to "solve"
+yoriuscacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        x <- x$getmean()
+        if (!is.null(a)){
+                message ("getting the average data")
+                return (a)
+                }
+        data <- x$get()
+        a <- solve(data, ...)
+        x$setmean(a)
+        v
 }
